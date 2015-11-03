a8795f89b7d2b363d666285b1cef140f7af71ee3
Peter Elam <elampj@gmail.com>
2015-11-03 09:13:33 +0000

    starting a tough day

---
+++ app/assets/javascripts/expandingScript.js
@@ -0,0 +1,35 @@
+
+$( ".expand-button" ).click(function() {
+
+  //$(this).siblings().children().each(function( index ) {
+    //$(this).next().children().each(function( index ) {
+    var currentId = $(this).attr('id');
+    $('.'+currentId).each(function( index ) {   
+      if ($( this ).hasClass('hidden'))
+      { 
+        $( this ).removeClass('hidden');
+      $( this ).addClass('expanded');
+    }
+    else if ($( this ).hasClass('expanded'))
+      { 
+        $( this ).removeClass('expanded');
+      $( this ).addClass('hidden');
+    }       
+
+  });
+
+  if ($( this ).hasClass('expand-button'))
+  {
+    //$( this ).removeClass('expand-button');
+    //$( this ).addClass('less-button');
+    $( this ).addClass('hidden');
+    
+  }
+  else if ($( this ).hasClass('less-button'))
+  {
+    $( this ).removeClass('less-button');
+    $( this ).addClass('expand-button');
+    //$( this ).html('&darr; More');
+  }
+
+});
