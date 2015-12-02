90377a4502536ff59540cff105a2b70d58e22717
Peter Elam <elampj@gmail.com>
2015-12-02 08:42:06 -0500

    getting started for the day

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: 'https://pedro-pinteresting.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://diamond-tree.herokuapp.com' }
 end
